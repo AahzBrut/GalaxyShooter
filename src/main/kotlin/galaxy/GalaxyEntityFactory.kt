fba9ceddb5ea181552a95da26c606962d265db66
AahzBrut <partyomenko@gmail.com>
2020-08-03 18:58:49 +0300

    Suppress false warnings

--- src/main/kotlin/galaxy/GalaxyEntityFactory.kt
+++ src/main/kotlin/galaxy/GalaxyEntityFactory.kt
@@ -13,13 +13,13 @@ import com.almasb.fxgl.entity.Spawns
 
 class GalaxyEntityFactory : EntityFactory {
 
+    @Suppress("UNUSED", "UNUSED_PARAMETER")
     @Spawns("Background")
     fun newBackground(data: SpawnData): Entity {
         return entityBuilder()
-            .at(-10.0, -10.0) // bigger than game size to account for camera shake
+            .at(-10.0, -10.0)
             .view(texture("SpaceBG_Overlay.png", 820.0, 620.0))
             .zIndex(-500)
             .build()
     }
-
 }
