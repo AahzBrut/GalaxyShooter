0cf5c71a439b8984f593d8b30937299583144b7a
AahzBrut <partyomenko@gmail.com>
2020-08-17 09:44:26 +0300

    Unbind managers from GUI update cycle

--- src/main/kotlin/galaxy/managers/EnemyManager.kt
+++ src/main/kotlin/galaxy/managers/EnemyManager.kt
@@ -8,12 +8,9 @@ import galaxy.entity_data.ENEMY
 import galaxy.entity_data.ENEMY_SPAWN
 import javafx.util.Duration
 
-class EnemyManager(private val spawnTimer: LocalTimer) {
+class EnemyManager() {
 
-    @Suppress("UNUSED_PARAMETER")
-    fun spawnEnemies(tpf: Double) {
-        if (!spawnTimer.elapsed(Duration.seconds(ENEMY_SPAWN.spawnDelay))) return
-        spawnTimer.capture()
+    fun spawnEnemies() {
 
         val spawnRange = IntRange(0, (getAppWidth() - ENEMY.size).toInt())
         spawn(GalaxyEntityType.ENEMY.typeName(), spawnRange.random().toDouble(), -ENEMY.size)
